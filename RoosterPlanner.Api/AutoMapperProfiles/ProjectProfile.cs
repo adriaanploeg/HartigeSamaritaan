497977443239560f7f5026954cf9e751f6d7027e
Ferdi <ferdivt@deltan-n.nl>
2019-11-08 23:36:11 +0100

    Added get project to API

---
+++ RoosterPlanner.Api/AutoMapperProfiles/ProjectProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+
+namespace RoosterPlanner.Api.AutoMapperProfiles
+{
+    public class ProjectProfile : Profile
+    {
+        public ProjectProfile()
+        {
+            CreateMap<RoosterPlanner.Models.Project, Models.Projects>();
+                //.ForMember(d => d.Name, opt => opt.MapFrom(src => src.Name));
+        }
+    }
+}
