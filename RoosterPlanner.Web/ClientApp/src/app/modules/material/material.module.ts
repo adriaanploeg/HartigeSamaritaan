2e538c0b25e7cf67c40c0b19161f5c70b4af9e45
Mitchel Hulst <mitchelhulst@gmail.com>
2019-11-08 22:15:05 +0100

    upgrade angular

--- RoosterPlanner.Web/ClientApp/src/app/modules/material/material.module.ts
+++ RoosterPlanner.Web/ClientApp/src/app/modules/material/material.module.ts
@@ -1,15 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    MatToolbarModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule
   ],
   exports: [
-    MatToolbarModule
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
-export class MaterialModule { }
+export class MaterialModule {}
