605070e3ab9a1fefdcd19d57bf2dac4f1581b4f1
Adriaan Ploeg <info@gertine-adriaan.nl>
2019-11-08 23:16:44 +0100

    added admin component

--- RoosterPlanner.Web/ClientApp/src/app/app-routing.module.ts
+++ RoosterPlanner.Web/ClientApp/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MsalGuard } from '@azure/msal-angular';
 import { HomeComponent } from './pages/home/home.component';
+import { AdminComponent } from './pages/admin/admin.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent, canActivate: [MsalGuard]},
+  { path: 'admin', component: AdminComponent, pathMatch: 'full' },
   { path: '**', redirectTo: '/' }
 ];
 
