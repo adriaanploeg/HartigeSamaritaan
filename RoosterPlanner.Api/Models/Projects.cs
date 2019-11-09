062081b8815b7142937e1fdb8ac9ca32240f860f
Ferdi <ferdivt@deltan-n.nl>
2019-11-09 05:48:13 +0100

    Added Person services

--- RoosterPlanner.Api/Models/Projects.cs
+++ RoosterPlanner.Api/Models/Projects.cs
@@ -7,7 +7,7 @@ namespace RoosterPlanner.Api.Models
 {
     public class Projects
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
